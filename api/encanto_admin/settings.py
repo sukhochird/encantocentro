7a7a46e60624539181d1ce92839324d87e282db9
dsukhochir <dsukhochir@gmail.com>
2025-12-16 17:44:16 +0800

    fix

--- api/encanto_admin/settings.py
+++ api/encanto_admin/settings.py
@@ -33,7 +33,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://encantocentro.mn",
     "https://encantocentro.vercel.app",
     "http://www.encantocentro.mn",
-    "https://www.encantocentro.mn"
+    "https://www.encantocentro.mn",
     "https://encantocentro.mn",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
